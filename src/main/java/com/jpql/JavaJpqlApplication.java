c56a4855d20ad15300df355eb5492abbb124abb8
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-22 03:44:59 +0700

    first commit

---
+++ src/main/java/com/jpql/JavaJpqlApplication.java
@@ -0,0 +1,13 @@
+package com.jpql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJpqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJpqlApplication.class, args);
+	}
+
+}
