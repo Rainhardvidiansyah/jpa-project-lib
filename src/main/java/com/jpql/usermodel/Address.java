17a5adbe5d78714647b204addd9e064ffa5b3341
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-22 18:51:38 +0700

    add user entity and implement User Detail in User.java

--- src/main/java/com/jpql/usermodel/Address.java
+++ src/main/java/com/jpql/usermodel/Address.java
@@ -1,16 +1,41 @@
 package com.jpql.usermodel;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "user_address")
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @Column(name = "nama_jalan", length = 300)
     private String street;
+
+    @Column(name = "Nomor_rumah", length = 225)
+    private String houseNumber;
+
+    @Column(name = "kelurahan", length = 100)
+    private String Urban_village;
+
+    @Column(name = "kecamatan", length = 100)
+    private String district;
+
+    @Column(name = "kodepos", length = 50)
+    private String zipCode;
     
 }
--- src/main/java/com/jpql/usermodel/User.java
+++ src/main/java/com/jpql/usermodel/User.java
@@ -1,5 +1,7 @@
 package com.jpql.usermodel;
 
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,28 +9,69 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter @Setter
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "first_name", length = 150, nullable = false)
-    private String firstName;
+    @Column(name = "user_name", length = 150, nullable = false)
+    private String nameofuser;
 
-    @Column(name = "last_name", length = 150, nullable = false)
-    private String lastName;
-
+    @Column(name = "user_email", length = 150, nullable = false)
     private String email;
 
+    @Column(name = "password_user", nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
     
     
