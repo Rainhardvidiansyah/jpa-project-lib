67862247c650ec43c4199738fdcac91bc9a6c523
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-23 00:35:28 +0700

    edit UserRepo.java

--- src/main/java/com/jpql/Repository/UserRepo.java
+++ src/main/java/com/jpql/Repository/UserRepo.java
@@ -1,11 +1,12 @@
-package com.jpql.Repository;
+package com.jpql.repository;
 
 import com.jpql.usermodel.User;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo {
+public interface UserRepo extends JpaRepository<User, Long>{
 
     public User findByEmail(String email);
     
