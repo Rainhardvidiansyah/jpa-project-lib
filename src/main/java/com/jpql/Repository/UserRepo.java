849892c08f3d6458459abc16598a397b64ed9f92
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-26 03:30:50 +0700

    add modifiying and query to enableUser = true in UserRepo

--- src/main/java/com/jpql/Repository/UserRepo.java
+++ src/main/java/com/jpql/Repository/UserRepo.java
@@ -5,11 +5,18 @@ import java.util.Optional;
 import com.jpql.usermodel.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long>{
 
     Optional <User> findByEmail(String email);
+
+    @Modifying
+    @Query("update User u set u.isEnabled = true where u.email = ?1")
+    int updateEnabledUserToTrue(String email);
+
     
 }
