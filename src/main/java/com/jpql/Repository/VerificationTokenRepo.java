387bafae75f7f40131cdc4dabfefe97ebb7c5209
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-26 03:17:51 +0700

    add modifiying and query to update date

--- src/main/java/com/jpql/Repository/VerificationTokenRepo.java
+++ src/main/java/com/jpql/Repository/VerificationTokenRepo.java
@@ -1,8 +1,15 @@
 package com.jpql.Repository;
 
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
 import com.jpql.usermodel.VerificationToken;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,6 +18,11 @@ public interface VerificationTokenRepo
 
         
         VerificationToken findByToken(String token);
+
+        @Transactional
+        @Modifying
+        @Query("UPDATE VerificationToken v SET v.confirmedAt = ?2 WHERE v.token = ?1")
+        int updateTokenConfirmedAtAndToken(String token, LocalDateTime confirmedAt);
 
     
 }
