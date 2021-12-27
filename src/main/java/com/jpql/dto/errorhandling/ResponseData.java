d78fabd62b353a31faf070e13ab290292c4adb98
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-27 20:52:07 +0700

    edit two files

---
+++ src/main/java/com/jpql/dto/errorhandling/ResponseData.java
@@ -0,0 +1,24 @@
+package com.jpql.dto.errorhandling;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor 
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseData<T> {
+
+    private boolean status;
+
+    private List<String> messages = new ArrayList<>();
+
+    private T payload;
+
+}
