b1440bc06498c4a02e1efebb50096dc4f00afcbd
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-27 22:36:37 +0700

    add registration functionality in controller

--- src/main/java/com/jpql/controller/registration/RegistrationController.java
+++ src/main/java/com/jpql/controller/registration/RegistrationController.java
@@ -1,15 +1,25 @@
 package com.jpql.controller.registration;
 
+import javax.validation.Valid;
+
+import com.jpql.dto.errorhandling.ResponseData;
+import com.jpql.dto.registration.UserRegisterDto;
 import com.jpql.service.UserService;
+import com.jpql.usermodel.User;
+import com.jpql.utilities.ErrorUtils;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/registration")
+@RequestMapping
 public class RegistrationController {
 
     @Autowired
@@ -18,8 +28,20 @@ public class RegistrationController {
     @Autowired
     private ModelMapper modelMapper;
 
-    public void registerUser(){
-        
+    @PostMapping("/registration")    
+    public ResponseEntity<?> registerUser(@Valid UserRegisterDto userRegisterDto, Errors errors){
+        ResponseData<UserRegisterDto> responseData = new ResponseData();
+        if(errors.hasErrors()){
+            responseData.setStatus(false);
+            responseData.setMessages(ErrorUtils.err(errors));
+            responseData.setPayload(null);
+            return ResponseEntity.badRequest().body(responseData);
+        }
+        User user = modelMapper.map(userRegisterDto, User.class);
+        userService.registration(user);
+        responseData.setStatus(true);
+        responseData.setPayload(modelMapper.map(user, UserRegisterDto.class));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     
