0fdd912aef575c14e78bafe4e3c6b1c1679f3c1e
Rainhardvidiansyah <rainhardvidians@gmail.com>
2021-12-28 21:27:52 +0700

    add new paramater in sendEmail method

--- src/main/java/com/jpql/service/email/EmailService.java
+++ src/main/java/com/jpql/service/email/EmailService.java
@@ -1,5 +1,7 @@
 package com.jpql.service.email;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,12 +13,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String content){
+    public void sendEmail(String to, String subject, String content, Date date){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(to);
         mailMessage.setSubject(subject);
         mailMessage.setText(content);
         mailMessage.setFrom("apps-no-reply@app.com");
+        mailMessage.setSentDate(date);
         mailSender.send(mailMessage);
     }
     
