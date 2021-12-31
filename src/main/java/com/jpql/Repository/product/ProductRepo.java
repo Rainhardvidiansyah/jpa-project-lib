1079d722a47ab8ed645cc862c7d37fed88a71f14
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-01 01:13:55 +0700

    Edit Query

--- src/main/java/com/jpql/Repository/product/ProductRepo.java
+++ src/main/java/com/jpql/Repository/product/ProductRepo.java
@@ -11,12 +11,21 @@ import org.springframework.data.repository.query.Param;
 
 public interface ProductRepo extends JpaRepository<ProductEntity, Long>{
 
+    @Query("select p from ProductEntity p where p.productName= :productName AND p.productDescription= :productDescription")
+    List<ProductEntity> findNameAndPrice(@Param("productName") String productName, @Param("productDescription") String productDescription);
+
+
+    //List<ProductEntity> findAllProductEntity(ProductEntity productEntity);
+
     @Query("select p from ProductEntity p where p.productName = :productName")
     List <ProductEntity> findByName(@Param("productName") String productName);
 
     //@Query("SELECT m FROM Movie m WHERE m.title LIKE %:title%")
     @Query("select p from ProductEntity p where p.productName like %:productName%")
     List<ProductEntity> findProductNameLike(String productName);
+
+    @Query("select p from ProductEntity p where p.productDescription like %:productDescription%")
+    List<ProductEntity> findByProductDescription(String productDescription);
 
 
 
