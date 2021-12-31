79e3ae286f7354d43d1d656f1133aa5b6a40e774
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-01 01:14:35 +0700

    Add Object Product

--- src/main/java/com/jpql/entities/product/ProductDB.java
+++ src/main/java/com/jpql/entities/product/ProductDB.java
@@ -13,20 +13,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductDB implements CommandLineRunner{
 
-    @Autowired
+    @Autowired(required = true)
     private ProductRepo productRepo;
 
     @Override
     public void run(String... args) throws Exception {
         productRepo.saveAll(List.of(
-            new ProductEntity(1l, "Sambal Matah", "Khas Bali", 20.000, 300,
+            new ProductEntity("Sambal Matah", "Khas Bali", 20.000, 300,
                 LocalDate.of(2021, Month.OCTOBER, 20), LocalDate.of(2021, Month.NOVEMBER, 12)),
-            new ProductEntity(2L, "Sambal Bawang", "Khas Jawa Timur", 35.000, 211,
+            new ProductEntity( "Sambal Bawang", "Khas Jawa Timur", 35.000, 211,
                 LocalDate.of(2021, Month.DECEMBER, 5), LocalDate.of(2022, Month.FEBRUARY, 20)),
-            new ProductEntity(3L, "Sambal Dabu-dabu", "Khas Manado", 45.000, 156,
+            new ProductEntity("Sambal Dabu-dabu", "Khas Manado", 45.000, 156,
                 LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.FEBRUARY, 20)),
-            new ProductEntity(4L, "Sambal ABC", "Khas Nasional", 15.000, 50,
-                LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.FEBRUARY, 20))
+            new ProductEntity("Sambal ABC", "Khas Nasional", 15.000, 50,
+                LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.MARCH, 20)),
+            new ProductEntity("Sambal JABAR", "Khas JAWA BARAT", 15.000, 87,
+                LocalDate.of(2022, Month.FEBRUARY, 5), LocalDate.of(2022, Month.APRIL, 30)),
+            new ProductEntity("Sambal JATENG", "Khas JAWA TENGAH", 100.000, 233,
+                LocalDate.of(2022, Month.FEBRUARY, 5), LocalDate.of(2022, Month.MAY, 12))
         ));
 
         System.out.println("DATA RECORDED");
