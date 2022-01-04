42c58cc821e0569bc419ab0f8793d0dc26243084
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-05 03:31:14 +0700

    edit TokenRepo.java

--- src/main/java/com/jpql/Repository/VerificationTokenRepo.java
+++ src/main/java/com/jpql/Repository/VerificationTokenRepo.java
@@ -4,14 +4,12 @@ package com.jpql.Repository;
 import java.time.LocalDateTime;
 
 import javax.transaction.Transactional;
-import javax.websocket.server.PathParam;
 
 import com.jpql.usermodel.VerificationToken;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
