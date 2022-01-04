9b8591e63ed7e1a15c98af746249880b3fbeaba6
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-05 03:29:13 +0700

    edit WebSecurityConfiguration

--- src/main/java/com/jpql/configuration/websecurity/WebSecurityConfiguration.java
+++ src/main/java/com/jpql/configuration/websecurity/WebSecurityConfiguration.java
@@ -6,7 +6,6 @@ import com.jpql.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,8 +43,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-        .authorizeRequests().antMatchers("/registration").permitAll()
-        .and().authorizeRequests().antMatchers("/test").permitAll()
+        .authorizeRequests().antMatchers("/**").permitAll()
+        // .antMatchers("registration").permitAll()
+        // .antMatchers("confirmation").permitAll()
         .anyRequest()
         .authenticated();
         
