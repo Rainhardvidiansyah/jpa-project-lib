20e32218df2090a66e0213a28526f29c24d20de3
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-08 01:52:44 +0700

    change column into user

--- src/main/java/com/jpql/service/address/AddressService.java
+++ src/main/java/com/jpql/service/address/AddressService.java
@@ -2,8 +2,10 @@ package com.jpql.service.address;
 
 import java.util.List;
 
+import com.jpql.Repository.UserRepo;
 import com.jpql.Repository.address.AddressRepo;
 import com.jpql.usermodel.Address;
+import com.jpql.usermodel.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddressService {
 
+    /*
+    THIS CLASS SHOULD CONTAIN:
+    1. USER ADD HIS ADDRESS
+    2, USER FIND HIS ADDRESS AND FIND HIS SECONDARY ADDRESS
+    */
+
     @Autowired
     private AddressRepo addressRepo;
 
+    @Autowired
+    private UserRepo userRepo;
+    
+
     public Address addNewAddress(Address address){
+        User user = new User();
+        user.setAddress(address);
         return addressRepo.save(address);
     }
 
