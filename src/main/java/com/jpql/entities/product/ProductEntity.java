799c7dff8a6ca2989d245e0f5402293f39f39aa7
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-07 23:10:35 +0700

    edit ProductEntity class

--- src/main/java/com/jpql/entities/product/ProductEntity.java
+++ src/main/java/com/jpql/entities/product/ProductEntity.java
@@ -1,19 +1,31 @@
 package com.jpql.entities.product;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.jpql.entities.cart.CartItems;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 @Getter
 @Setter
 @Table(name = "product_tbl")
@@ -26,6 +38,9 @@ public class ProductEntity {
 
     private String productDescription;
 
+    @Enumerated(EnumType.STRING)
+    private Level level;
+
     private Double price;
 
     private int stocks;
@@ -34,8 +49,22 @@ public class ProductEntity {
 
     private LocalDate expiredAt;
 
-    public ProductEntity(String productName, String productDescription, Double price, int stocks, LocalDate madeAt,
-            LocalDate expiredAt) {
+    @OneToMany(mappedBy = "productEntity", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<CartItems> cartItems;
+
+    public Level showLevel(){
+        for(Level level : Level.values()){
+            level.toString();
+        }
+        return level;
+    }
+
+
+
+    public ProductEntity(String productName, String productDescription, 
+                        Double price, int stocks, LocalDate madeAt, 
+                        LocalDate expiredAt) {
         this.productName = productName;
         this.productDescription = productDescription;
         this.price = price;
