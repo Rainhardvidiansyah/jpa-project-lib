fa40efa8cb75e4076fd79d3ecd589f9c65cf723d
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-07 23:24:23 +0700

    make CartService Class and injecting repository from CartRepo and ProductService

---
+++ src/main/java/com/jpql/service/cart/CartService.java
@@ -0,0 +1,28 @@
+package com.jpql.service.cart;
+
+
+import com.jpql.Repository.cart.CartRepo;
+import com.jpql.service.product.ProductService;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    /**
+     THIS FILE SHOULD HANDLE:
+     1. PRODUCT CHOSEN BY USER
+     2. PRODUCT ADDED TO CART BY USER
+     */
+
+    @Autowired
+    private CartRepo cartRepo;
+
+    @Autowired
+    private ProductService productService;
+
+
+}
+
