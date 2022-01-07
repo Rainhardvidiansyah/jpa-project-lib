57591d819577364d5e2527def69eda7a08e7de9e
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-01-07 23:15:24 +0700

    add User Address Repo

---
+++ src/main/java/com/jpql/Repository/address/AddressRepo.java
@@ -0,0 +1,10 @@
+package com.jpql.Repository.address;
+
+import com.jpql.usermodel.Address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepo extends JpaRepository<Address, Long>{
+
+}
+    
