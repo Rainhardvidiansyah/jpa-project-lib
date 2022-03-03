cf30314518c5f4d0bde5add4d24234c32ac5b3b2
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-03-03 19:46:38 +0700

    refactoring few files

--- src/main/java/com/jpql/entities/product/ProductDB.java
+++ src/main/java/com/jpql/entities/product/ProductDB.java
@@ -22,22 +22,22 @@ public class ProductDB implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        // productRepo.saveAll(List.of(
-        //     new ProductEntity("Sambal Matah", "Khas Bali", 20.000, 300,
-        //         LocalDate.of(2021, Month.OCTOBER, 20), LocalDate.of(2021, Month.NOVEMBER, 12)),
-        //     new ProductEntity( "Sambal Bawang", "Khas Jawa Timur", 35.000, 211,
-        //         LocalDate.of(2021, Month.DECEMBER, 5), LocalDate.of(2022, Month.FEBRUARY, 20)),
-        //     new ProductEntity("Sambal Dabu-dabu", "Khas MANADO", 45.000, 156,
-        //         LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.FEBRUARY, 20)),
-        //     new ProductEntity("Sambal ABC", "Khas NASIONAL", 15.000, 50,
-        //         LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.MARCH, 20)),
-        //     new ProductEntity("Sambal JABAR", "Khas JAWA BARAT", 15.000, 87,
-        //         LocalDate.of(2022, Month.FEBRUARY, 5), LocalDate.of(2022, Month.APRIL, 30)),
-        //     new ProductEntity("Sambal JATENG", "Khas JAWA TENGAH", 100.000, 233,
-        //         LocalDate.of(2022, Month.FEBRUARY, 5), LocalDate.of(2022, Month.MAY, 12)),
-        //     new ProductEntity("Sambal DKI", "Khas JAKARTA", 60.000, 780,
-        //         LocalDate.of(2022, Month.MARCH, 5), LocalDate.of(2022, Month.MAY, 31)) 
-        // ));
+        productRepo.saveAll(List.of(
+            new ProductEntity("Sambal Matah", "Khas Bali", 20.500, 300,
+                LocalDate.of(2021, Month.OCTOBER, 20), LocalDate.of(2021, Month.NOVEMBER, 12)),
+            new ProductEntity( "Sambal Bawang", "Khas Jawa Timur", 35.200, 211,
+                LocalDate.of(2021, Month.DECEMBER, 5), LocalDate.of(2022, Month.FEBRUARY, 20)),
+            new ProductEntity("Sambal Dabu-dabu", "Khas MANADO", 45.000, 156,
+                LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.FEBRUARY, 20)),
+            new ProductEntity("Sambal ABC", "Khas NASIONAL", 15.000, 50,
+                LocalDate.of(2022, Month.JANUARY, 10), LocalDate.of(2022, Month.MARCH, 20)),
+            new ProductEntity("Sambal JABAR", "Khas JAWA BARAT", 15.000, 87,
+                LocalDate.of(2022, Month.FEBRUARY, 5), LocalDate.of(2022, Month.APRIL, 30)),
+            new ProductEntity("Sambal JATENG", "Khas JAWA TENGAH", 100.000, 233,
+                LocalDate.of(2022, Month.FEBRUARY, 5), LocalDate.of(2022, Month.MAY, 12)),
+            new ProductEntity("Sambal DKI", "Khas JAKARTA", 60.000, 780,
+                LocalDate.of(2022, Month.MARCH, 5), LocalDate.of(2022, Month.MAY, 31)) 
+        ));
         LOGGER.info("DATA PRODUCT RECORDED");
         
     }
--- src/main/java/com/jpql/usermodel/AdminDb.java
+++ src/main/java/com/jpql/usermodel/AdminDb.java
@@ -1,14 +1,12 @@
 package com.jpql.usermodel;
 
 import java.util.Arrays;
-import java.util.List;
 
 import com.jpql.Repository.UserRepo;
 import com.jpql.Repository.role.RoleRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.support.BeanDefinitionDsl.Role;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -26,11 +24,11 @@ public class AdminDb implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        // userRepo.saveAll(Arrays.asList(
-        //     new User("USER", "user@gmail.com", passwordEncoder.encode("user")),
-        //     new User("ADMIN", "admin@gmail.com", passwordEncoder.encode("admin")),
-        //     new User("DEV", "dev@gmail.com", passwordEncoder.encode("dev"))
-        // ));
+        userRepo.saveAll(Arrays.asList(
+            new User("USER", "user@gmail.com", passwordEncoder.encode("user")),
+            new User("ADMIN", "admin@gmail.com", passwordEncoder.encode("admin")),
+            new User("DEV", "dev@gmail.com", passwordEncoder.encode("dev"))
+        ));
     }
 
 
