54939d927785c48ee9a5b49802204fb292037cb6
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-04-11 13:22:49 +0700

    add cross origin

--- src/main/java/com/jpql/controller/registration/RegistrationController.java
+++ src/main/java/com/jpql/controller/registration/RegistrationController.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/api/auth")
 public class RegistrationController {
 
